3b5dcf8d8cb2b5f0b2d3418aecf9361ee77106f5
fpikus <39199330+fpikus@users.noreply.github.com>
2026-09-02 23:18:20 -0700

    Add files via upload

--- Chapter10/19_measure_dynamic.C
+++ Chapter10/19_measure_dynamic.C
@@ -10,18 +10,18 @@ class Shape {
 void measure(const std::vector<Shape>& VS,
         double* length, double* width, double* depth,
         double* volume, double* weight) {
+  double l, w, d, v;
   for (const Shape& S : VS) {
-    double l, w, d, v;
     if (weight && !volume) volume = &v;
     if (volume) {
       if (!length) length = &l;
       if (!width ) width  = &w;
       if (!depth ) depth  = &d;
     }
-    if (length) *length = S.l();
-    if (width ) *width  = S.w();
-    if (depth ) *depth  = S.d();
-    if (volume) *volume = *length * *width * *depth;
-    if (weight) *weight = *volume * S.density();
+    if (length) *length += S.l();
+    if (width ) *width  += S.w();
+    if (depth ) *depth  += S.d();
+    if (volume) *volume += *length * *width * *depth;
+    if (weight) *weight += *volume * S.density();
   }
 }
